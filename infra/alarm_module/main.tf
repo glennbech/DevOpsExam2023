b5699608dcc8a4d8b6a1c4f75fc3a4a34b959738
Marcus <60523971+Simpada@users.noreply.github.com>
2023-11-18 23:50:34 +0100

    Try p90.0 as stat

--- infra/alarm_module/main.tf
+++ infra/alarm_module/main.tf
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_metric_alarm" "threshold" {
   threshold = var.threshold
   evaluation_periods = "2"
   period = "60"
-  extended_statistic = "p90.0"
+  stat = "p90.0"
 
   alarm_description = "This alarm goes of, when a call at the 99th percentile exceeds the threshold"
   alarm_actions = [aws_sns_topic.user_updates.arn]
